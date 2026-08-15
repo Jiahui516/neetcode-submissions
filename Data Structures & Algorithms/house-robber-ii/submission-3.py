6e53a97631354d7146b9d9e8a88b840817a48353
Jiahui Wang <143573352+Jiahui516@users.noreply.github.com>
2026-08-15 00:41:54 -0400

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return nums[0]
+
+        def robLine(arr):
+            if len(arr)==1:
+                return arr[0]
+
+            dp=[0]*len(arr)
+
+            dp[0]=arr[0]
+            dp[1]=max(arr[0],arr[1])
+
+            for i in range(2,len(arr)):
+                dp[i]=max(
+                    arr[i]+dp[i-2],
+                    dp[i-1]
+                )
+            
+            return dp[-1]
+        
+        return max(
+            robLine(nums[1:]),
+            robLine(nums[:-1])
+        )
+
+            
