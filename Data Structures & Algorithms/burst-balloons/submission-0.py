a76990444b15a5ec01cc34f8a4d0604baf1100f9
Jiahui Wang <143573352+Jiahui516@users.noreply.github.com>
2026-08-31 20:22:29 -0400

    Add: burst-balloons - submission-0

---
+++ Data Structures & Algorithms/burst-balloons/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        nums=[1]+nums+[1]
+        memo={}
+
+        def dfs(left,right):
+            if left+1==right:
+                return 0
+            
+            if (left,right) in memo:
+                return memo[(left,right)]
+
+            ans =0
+
+            for i in range(left+1,right):
+                coins =(
+                    dfs(left,i)+
+                    nums[left]*nums[i]*nums[right]+
+                    dfs(i,right)
+                )
+
+                ans=max(ans,coins)
+            memo[(left,right)]=ans
+            return ans
+        
+        return dfs(0,len(nums)-1)
+        
